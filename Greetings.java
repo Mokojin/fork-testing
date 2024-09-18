8b58d551681243dc96c8418f5650ece611dccec8
Lloyd <agdanlloyd@gmail.com>
2024-09-18 18:57:09 +0800

    Added multiple statement

--- Greetings.java
+++ Greetings.java
@@ -3,5 +3,7 @@ public class Greetings {
     public static void main(String[] args) {
         System.out.println("Hello World Fork!");
         System.out.println("Added using fork!!");
+        System.out.println("Added using fork!!");
+        System.out.println("Added using fork!!");
     }
 }
